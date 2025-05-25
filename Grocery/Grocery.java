d6bb2653fe77d28798b618217cc691861512d4b6
Dan <141607124+Nachtr@users.noreply.github.com>
2025-05-25 06:21:38 -0500

    Add files via upload

---
+++ Grocery/Grocery.java
@@ -0,0 +1,11 @@
+public class Grocery {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Week 1 Groceries Bill: " + 150.00);
+        System.out.println("Week 2 Groceries Bill: " + 124.00);
+        System.out.println("Week 3 Groceries Bill: " + 178.00);
+        System.out.println("Week 4 Groceries Bill: " + 216.00);
+
+        System.out.println("Grocery Average: $" + ((150.0 + 124.00 + 178.00 + 216.00)/4));
+    }
+}
